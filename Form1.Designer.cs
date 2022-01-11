33dd5bc260680e3f6e011024f12df94c63344d43
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-01-11 16:31:06 +0800

    patch textbox,button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,24 +29,23 @@ namespace csharp_example
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.LblShow = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.TxtPW = new System.Windows.Forms.TextBox();
+            this.TxtMoney = new System.Windows.Forms.TextBox();
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // LblShow
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.LblShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblShow.Location = new System.Drawing.Point(41, 32);
+            this.LblShow.Name = "LblShow";
+            this.LblShow.Size = new System.Drawing.Size(309, 29);
+            this.LblShow.TabIndex = 0;
             // 
             // label2
             // 
@@ -66,60 +65,60 @@ namespace csharp_example
             this.label3.TabIndex = 0;
             this.label3.Text = "提領金額:";
             // 
-            // textBox1
+            // TxtPW
             // 
-            this.textBox1.Location = new System.Drawing.Point(134, 90);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
-            this.textBox1.TabIndex = 1;
+            this.TxtPW.Location = new System.Drawing.Point(134, 90);
+            this.TxtPW.Name = "TxtPW";
+            this.TxtPW.Size = new System.Drawing.Size(100, 25);
+            this.TxtPW.TabIndex = 1;
             // 
-            // textBox2
+            // TxtMoney
             // 
-            this.textBox2.Location = new System.Drawing.Point(134, 129);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 25);
-            this.textBox2.TabIndex = 1;
+            this.TxtMoney.Location = new System.Drawing.Point(134, 129);
+            this.TxtMoney.Name = "TxtMoney";
+            this.TxtMoney.Size = new System.Drawing.Size(100, 25);
+            this.TxtMoney.TabIndex = 1;
             // 
-            // button1
+            // BtnCheck
             // 
-            this.button1.Location = new System.Drawing.Point(275, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnCheck.Location = new System.Drawing.Point(275, 89);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(75, 23);
+            this.BtnCheck.TabIndex = 2;
+            this.BtnCheck.Text = "Check";
+            this.BtnCheck.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // BtnOK
             // 
-            this.button2.Location = new System.Drawing.Point(275, 124);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BtnOK.Location = new System.Drawing.Point(275, 124);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(75, 23);
+            this.BtnOK.TabIndex = 2;
+            this.BtnOK.Text = "OK";
+            this.BtnOK.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // BtnCancel
             // 
-            this.button3.Location = new System.Drawing.Point(275, 153);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button1";
-            this.button3.UseVisualStyleBackColor = true;
+            this.BtnCancel.Location = new System.Drawing.Point(275, 153);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 2;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(404, 213);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnOK);
+            this.Controls.Add(this.BtnCheck);
+            this.Controls.Add(this.TxtMoney);
+            this.Controls.Add(this.TxtPW);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblShow);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -129,14 +128,14 @@ namespace csharp_example
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblShow;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox TxtPW;
+        private System.Windows.Forms.TextBox TxtMoney;
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.Button BtnOK;
+        private System.Windows.Forms.Button BtnCancel;
     }
 }
 

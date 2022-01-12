d16850d4ee92e46a69fb511d97268a7504bfe624
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-01-12 17:27:51 +0800

    patch

--- Form1.cs
+++ Form1.cs
@@ -39,7 +39,51 @@ namespace csharp_example
                 LblMsg.Text = "歡迎使用本系統";
                 if(TbcOrder.SelectedIndex == 3 )
                 {
+                    string order = "";
+                    int qty = 0, total = 0;
+                    LblTotal.Text = order;
 
+                    if(ChkM1.Checked == true)
+                    {
+                        try 
+                        { qty = int.Parse(TxtM1.Text); }
+                        catch
+                        { LblMsg.Text = "請輸入正整數 !"; qty = 0; return; }
+
+                        total += qty * 20;
+                        order += $"蛋餅: {qty}份 {qty * 20} 元\n";
+                    }
+                    if(ChkM2.Checked == true)
+                    {
+                        try
+                        { qty = int.Parse(TxtM2.Text); }
+                        catch
+                        { LblMsg.Text = "請輸入正整數 !"; qty = 0; return; }
+
+                        total += qty * 30;
+                        order += $"三明治: {qty}份 {qty * 30} 元\n";
+                    }
+                    if(ChkR1.Checked == true)
+                    {
+                        try
+                        { qty = int.Parse(TxtR1.Text); }
+                        catch
+                        { LblMsg.Text = "請輸入正整數 !"; qty = 0; return; }
+
+                        total += qty * 15;
+                        order += $"豆漿: {qty}份 {qty * 15} 元\n";
+                    }
+                    if (ChkR2.Checked == true)
+                    {
+                        try
+                        { qty = int.Parse(TxtR2.Text); }
+                        catch
+                        { LblMsg.Text = "請輸入正整數 !"; qty = 0; return; }
+
+                        total += qty * 20;
+                        order += $"奶茶: {qty}份 {qty * 20} 元\n";
+                    }
+                    LblTotal.Text = $"點餐清單: \n {order}\n 總價: {total} 元";
                 }
             }
             else

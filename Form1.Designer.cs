c9eedfb14270f535b987850c6a1b2b222f2646e9
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-01-13 10:50:17 +0800

    add ComboBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,93 @@ namespace csharp_example
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CboList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnRemove = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CboList
+            // 
+            this.CboList.FormattingEnabled = true;
+            this.CboList.Location = new System.Drawing.Point(47, 65);
+            this.CboList.Name = "CboList";
+            this.CboList.Size = new System.Drawing.Size(121, 23);
+            this.CboList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "台灣百岳清單";
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(265, 29);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 2;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            // 
+            // BtnRemove
+            // 
+            this.BtnRemove.Location = new System.Drawing.Point(265, 80);
+            this.BtnRemove.Name = "BtnRemove";
+            this.BtnRemove.Size = new System.Drawing.Size(75, 23);
+            this.BtnRemove.TabIndex = 3;
+            this.BtnRemove.Text = "Remove";
+            this.BtnRemove.UseVisualStyleBackColor = true;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(265, 128);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 4;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            // 
+            // BtnSort
+            // 
+            this.BtnSort.Location = new System.Drawing.Point(265, 176);
+            this.BtnSort.Name = "BtnSort";
+            this.BtnSort.Size = new System.Drawing.Size(75, 23);
+            this.BtnSort.TabIndex = 5;
+            this.BtnSort.Text = "Sort";
+            this.BtnSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(448, 282);
+            this.Controls.Add(this.BtnSort);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnRemove);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CboList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox CboList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnRemove;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnSort;
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # csharp_example
+
+## ComboxBox
+
+### 常用屬性
+
+- MaxDropDownItems: 下拉式清單中可以顯示項目的最多個數
+- DropDownStyle
+Simple:           沒有下拉鈕，所以項目清單一直顯示,且使用者可以輸入文字
+DropDown(預設值): 按下拉鈕才會顯示項目清單，且使用者可以輸入文字
+DropDownList:    按下拉鈕才會顯示項目清單，但使用者不可以輸入文字

cb211c481d313a2491a355a7bf9a5ac4b9b75d3b
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-01-25 17:43:20 +0800

    add Newtonsoft.Json.dll and patch

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace csharp_example
 {
@@ -15,6 +16,34 @@ namespace csharp_example
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Age", Type.GetType("System.Int32"));
+            dt.Columns.Add("Name", Type.GetType("System.String"));
+            dt.Columns.Add("Sex", Type.GetType("System.String"));
+            dt.Columns.Add("IsMarry", Type.GetType("System.Boolean"));
+            for (int i = 0; i < 4; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Age"] = i + 1;
+                dr["Name"] = "Name" + i;
+                dr["Sex"] = i % 2 == 0 ? "男" : "女";
+                dr["IsMarry"] = i % 2 > 0 ? true : false;
+                dt.Rows.Add(dr);
+            }
+            dataGridView1.DataSource = dt;
+            Console.WriteLine(JsonConvert.SerializeObject(dt));
+
+            string json = JsonConvert.SerializeObject(dt);
+            DataTable dt2 = JsonConvert.DeserializeObject<DataTable>(json);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}",dr[0],dr[1],dr[2],dr[3]);
+            }
+            dataGridView2.DataSource = dt2;
         }
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,40 @@
 [Newtonsoft.Json高级用法][1]
 Json.Net是支持序列化和反序列化DataTable,DataSet,Entity Framework和Entity的。下面分别举例说明序列化和反序列化。
 
+用法
+```
+using Newtonsoft.Json;
+```
+
+1-1 JsonConvert.SerializeObject
+
+SerializeObject(Object)	
+Serializes the specified object to a JSON string. 
+
+將datatable轉為json格式如下
+
+```
+[{"Age":1,"Name":"Name0","Sex":"男","IsMarry":false},{"Age":2,"Name":"Name1","Sex":"女","IsMarry":true},{"Age":3,"Name":"Name2","Sex":"男","IsMarry":false},{"Age":4,"Name":"Name3","Sex":"女","IsMarry":true}]
+```
+
+1-2 JsonConvert.DeserializeObject
+
+
+
+JsonConvertDeserializeObjectT Method (String)
+
+```
+DataTable dt2 = JsonConvert.DeserializeObject<DataTable>(json);
+```
+
+解為DataTable資料內容如下
+```
+1	Name0	男	False
+2	Name1	女	True
+3	Name2	男	False
+4	Name3	女	True
+```
+
 [Json.NET Documentation][2]
 
 [1]:https://www.cnblogs.com/yanweidie/p/4605212.html

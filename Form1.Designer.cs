52f675864f832b4fe295df64a6c4fd1c987aa149
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-01-25 17:02:41 +0800

    patch example

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,16 @@ namespace csharp_example
             this.BtnNewRow = new System.Windows.Forms.Button();
             this.BtnDataRow1 = new System.Windows.Forms.Button();
             this.BtnCreate = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -76,11 +85,99 @@ namespace csharp_example
             this.BtnCreate.UseVisualStyleBackColor = true;
             this.BtnCreate.Click += new System.EventHandler(this.BtnCreate_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(98, 487);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(628, 295);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CreateTable";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 391);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "AddRow";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(319, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Print";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(511, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 34);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Remove All";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(413, 388);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 33);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Remove1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(98, 446);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 35);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Create";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(212, 448);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(89, 32);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Accept";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(715, 637);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.BtnCreate);
             this.Controls.Add(this.BtnDataRow1);
             this.Controls.Add(this.BtnNewRow);
@@ -89,6 +186,7 @@ namespace csharp_example
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +197,14 @@ namespace csharp_example
         private System.Windows.Forms.Button BtnNewRow;
         private System.Windows.Forms.Button BtnDataRow1;
         private System.Windows.Forms.Button BtnCreate;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -71,5 +71,132 @@ namespace csharp_example
             BtnDataRow1.Enabled = true;
             BtnNewRow.Enabled = true;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable("new-table");
+            dataGridView2.DataSource = dt;
+
+            DataColumn dc;
+            dc = new DataColumn("column0", typeof(String));
+            dt.Columns.Add(dc);
+            dc = new DataColumn("column1",typeof(DateTime));
+            dt.Columns.Add(dc);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Add("S00" + (++G_studentID), DateTime.Now);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach(DataColumn col in dt.Columns)
+            {
+                Console.WriteLine(col.ToString());
+            }
+            foreach(DataRow row in dt.Rows)
+            {
+                Console.WriteLine($"[{i++}] = {row[0].ToString()} , {row[1].ToString()}");
+            }
+            string name = dt.Rows[0][0].ToString();
+            string time = dt.Rows[0]["column1"].ToString();
+            Console.WriteLine("name" + name);
+            Console.WriteLine("time" + time);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for(int i= dt.Rows.Count-1; i>=0; i--)
+            {
+                dt.Rows.RemoveAt(i);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("0 count:" + dt.Rows.Count);
+            dt.Rows[1].Delete();
+            Console.WriteLine("1 count:" + dt.Rows.Count);
+            dt.AcceptChanges();
+            Console.WriteLine("2 count:" + dt.Rows.Count);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DataTable table;
+            table = MakeNameTable();
+           
+            DataRow row;
+            row = table.NewRow();
+
+            row["fName"] = "John";
+            row["lName"] = "Smith";
+            table.Rows.Add(row);
+
+            foreach (DataColumn column in table.Columns)
+                Console.WriteLine(column.ColumnName);
+
+            dataGridView2.DataSource = table;
+        }
+
+        private DataTable MakeNameTable()
+        {
+            DataTable namesTable = new DataTable("Names");
+            DataColumn idColumn = new DataColumn();
+            idColumn.DataType = System.Type.GetType("System.Int32");
+            idColumn.ColumnName = "id";
+            idColumn.AutoIncrement = true;
+            namesTable.Columns.Add(idColumn);
+
+            DataColumn fNameColumn = new DataColumn();
+            fNameColumn.DataType = System.Type.GetType("System.String");
+            fNameColumn.ColumnName = "Fname";
+            fNameColumn.DefaultValue = "Fname";
+            namesTable.Columns.Add(fNameColumn);
+
+            DataColumn lNameColumn = new DataColumn();
+            lNameColumn.DataType = System.Type.GetType("System.String");
+            lNameColumn.ColumnName = "LName";
+            namesTable.Columns.Add(lNameColumn);
+
+            DataColumn[] keys = new DataColumn[1];
+            keys[0] = idColumn;
+            namesTable.PrimaryKey = keys;
+            return namesTable;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DataTable table;
+            table = MakeTable();
+
+            DataRow row;
+            row = table.NewRow();
+
+            Console.WriteLine("New Row", row.RowState);
+
+            table.Rows.Add(row);
+            Console.WriteLine("AddRow " + row.RowState);
+            table.AcceptChanges();
+            // Unchanged row.
+            Console.WriteLine("AcceptChanges " + row.RowState);
+            row["FirstName"] = "Scott";
+            // Modified row.
+            Console.WriteLine("Modified " + row.RowState);
+            row.Delete();
+            // Deleted row.
+            Console.WriteLine("Deleted " + row.RowState);
+
+        }
+        private DataTable MakeTable()
+        {
+            DataTable table = new DataTable("table");
+            DataColumn fnameColumn = new DataColumn(
+                "FirstName", Type.GetType("System.String"));
+            table.Columns.Add(fnameColumn);
+            return table;
+        }
     }
 }

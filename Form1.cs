0c120585f0ab6a410fdd7344b2d9d91a10539f82
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-02-10 13:30:29 +0800

    patch NLog example

--- Form1.cs
+++ Form1.cs
@@ -11,9 +11,21 @@ namespace csharp_example
 {
     public partial class Form1 : Form
     {
+        private static NLog.Logger Logger = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Logger = NLog.LogManager.GetCurrentClassLogger();
+            Logger.Fatal("Hello {0}", "Fatal");
+            Logger.Error("Hello {0}", "Error");
+            Logger.Warn("Hello {0}", "Warn");
+            Logger.Info("Hello {0}", "Info");
+            Logger.Debug("Hello {0}", "Debug");
+            Logger.Trace("Hello {0}", "Trace");
         }
     }
 }

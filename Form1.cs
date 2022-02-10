5035dd32c350c00e62b4f9e98c2419473a76d041
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-02-10 14:27:22 +0800

    patch NLog richTextBox,verify ok

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,28 @@ namespace csharp_example
 {
     public partial class Form1 : Form
     {
+        private static NLog.Logger Logger = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NLog.Windows.Forms.RichTextBoxTarget target = new NLog.Windows.Forms.RichTextBoxTarget();
+            target.Layout = "${date:format=HH\\:MM\\:ss} ${logger} ${message}";
+            target.ControlName = "richTextBox1";
+            target.FormName = "Form1";
+            target.UseDefaultRowColoringRules = true;
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, NLog.LogLevel.Trace);
+
+            Logger = NLog.LogManager.GetCurrentClassLogger();
+            Logger.Fatal("Hello {0}", "Fatal");
+            Logger.Error("Hello {0}", "Error");
+            Logger.Warn("Hello {0}", "Warn");
+            Logger.Info("Hello {0}", "Info");
+            Logger.Debug("Hello {0}", "Debug");
+            Logger.Trace("Hello {0}", "Trace");
         }
     }
 }

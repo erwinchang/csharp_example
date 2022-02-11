f5a7fb2b5c562d025f9f638639c054add2d0f360
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-02-11 11:08:47 +0800

    patch HttpPostEx, need verify

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.IO;
+using System.Web.Script.Serialization;
 
 namespace csharp_example
 {
@@ -17,7 +18,7 @@ namespace csharp_example
         public Form1()
         {
             InitializeComponent();
-            HttpGetEx();
+            HttpPostEx();
         }
 
         //https://blog.yowko.com/webrequest-and-httpwebrequest/
@@ -27,6 +28,30 @@ namespace csharp_example
             request.Method = "GET";
             using (var httpResponse = (HttpWebResponse)request.GetResponse())
 
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine($"result:{result}");
+            }
+        }
+
+        //https://reqbin.com/req/4rwevrqh/post-json-example
+        public static void HttpPostEx()
+        {
+            WebRequest request = WebRequest.Create("https://reqbin.com/echo/post/json");
+            request.Method = "POST";
+            PostData postData = new PostData() { userId = 1, title = "yowko", body = "yowko test body 中文" };
+            request.ContentType = "application/json; charset=utf-8";
+            request.Headers.Add("authorization", "token apikey");
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = new JavaScriptSerializer().Serialize(postData);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+            }
+
+            using (var httpResponse = (HttpWebResponse)request.GetResponse())
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
                 var result = streamReader.ReadToEnd();
---
+++ PostData.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_example
+{
+    internal class PostData
+    {
+        public PostData()
+        {
+        }
+
+        public int userId { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}

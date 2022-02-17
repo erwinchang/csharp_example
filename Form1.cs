362cc2aec6924b1f79c9505ff438968df863d6bd
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-02-17 11:37:51 +0800

    patch example

--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,7 @@ namespace csharp_example
         {
             InitializeComponent();
             dataGridView1.Dock = DockStyle.Fill;
-            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
 
             Controls.Add(dataGridView1);
 
@@ -26,6 +26,40 @@ namespace csharp_example
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            testTask.Add(new TestPlanTask(1, "XTT1-FILE-NAME",25.0,3.1));
+            testTask.Add(new TestPlanTask(2, "XTT2-FILE-NAME", 25.0, 3.2));
+            testTask.Add(new TestPlanTask(3, "XTT3-FILE-NAME", 25.0, 3.3));
+            testTask.Add(new TestPlanTask(4, "XTT4-FILE-NAME----------", 25.0, 3.4));
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = testTask;
+            AddColumns();
+        }
+
+        private void AddColumns()
+        {
+            DataGridViewTextBoxColumn idColumn = new DataGridViewTextBoxColumn();
+            idColumn.Name = "CaseID";
+            idColumn.DataPropertyName = "Id";
+            idColumn.ReadOnly = true;
+
+            DataGridViewTextBoxColumn xttFileNameColumn = new DataGridViewTextBoxColumn();
+            xttFileNameColumn.Name = "XTT File Name";
+            xttFileNameColumn.DataPropertyName = "XTTFileName";
+            xttFileNameColumn.ReadOnly = true;
+
+            DataGridViewTextBoxColumn temperatureValueColumn = new DataGridViewTextBoxColumn();
+            temperatureValueColumn.Name = "Temperature";
+            temperatureValueColumn.DataPropertyName = "TemperatureValue";
+
+            DataGridViewTextBoxColumn voltageValueColumn = new DataGridViewTextBoxColumn();
+            voltageValueColumn.Name = "Voltage";
+            voltageValueColumn.DataPropertyName = "VoltageValue";
+
+            dataGridView1.Columns.Add(idColumn);
+            dataGridView1.Columns.Add(xttFileNameColumn);
+            dataGridView1.Columns.Add(temperatureValueColumn);
+            dataGridView1.Columns.Add(voltageValueColumn);
         }
     }
 
@@ -33,10 +67,10 @@ namespace csharp_example
 
 public class TestPlanTask
 {
-    public int Id;
-    public String XTTFileName;
-    public double TemperatureValue;
-    public double VoltageValue;
+    public int Id { get; set; }
+    public String XTTFileName { get; set; }
+    public double TemperatureValue { get; set; }
+    public double VoltageValue { get; set; }
 
     public TestPlanTask(int id, String xttFileName, double temperature,double voltage)
     {
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # csharp_example
+
+## DataGridView example
+
+
+```
+public class TestPlanTask
+{
+    public int Id { get; set; }
+    public String XTTFileName { get; set; }
+    public double TemperatureValue { get; set; }
+    public double VoltageValue { get; set; }
+
+    public TestPlanTask(int id, String xttFileName, double temperature,double voltage)
+    {
+        Id = id;
+        XTTFileName = xttFileName;
+        TemperatureValue = temperature;
+        VoltageValue = voltage;
+    }
+}
+```
+
+![Imgur](https://i.imgur.com/7ydkySm.png)

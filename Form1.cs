54e8b3fa9a8417f04775655abf1f37690b4ceb67
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-02-17 10:35:56 +0800

    add DataGridView

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,31 @@ namespace csharp_example
 {
     public partial class Form1 : Form
     {
+        private List<TestPlanTask> testTask = new List<TestPlanTask>();
+        private DataGridView dataGridView1 = new DataGridView();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.Dock = DockStyle.Fill;
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            Controls.Add(dataGridView1);
         }
+    }
+}
+
+public class TestPlanTask
+{
+    public int Id;
+    public String XTTFileName;
+    public double TemperatureValue;
+    public double VoltageValue;
+
+    public TestPlanTask(int id, String xttFileName, double temperature,double voltage)
+    {
+        Id = id;
+        XTTFileName = xttFileName;
+        TemperatureValue = temperature;
+        VoltageValue = voltage;
     }
 }

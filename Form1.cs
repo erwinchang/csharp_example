3d09a04156b9448178688458ea6b64cf61d001ba
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-03-07 15:55:32 +0800

    Patch example

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Xsl;
 
 namespace csharp_example
 {
@@ -15,6 +16,19 @@ namespace csharp_example
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new XslCompiledTransform and load the compiled transformation.
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(typeof(Transform));
+
+            String srcXmlFile = @"D:\gitWork\test-xml-xslt\books.xml";
+            String resHtmlFile = @"D:\gitWork\test-xml-xslt\discount_books.html";
+            // Execute the transformation and output the results to a file.
+            //xslt.Transform("books.xml", "discount_books.html");
+            xslt.Transform(srcXmlFile, resHtmlFile);
         }
     }
 }

eaa87ba35411af6a9101325e26d79e20b38b1577
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-03-11 12:16:31 +0800

    Patch example

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # csharp_example
+
+## [如何從 Microsoft Visual C#.NET 自動化 Microsoft Excel][1]
+
+執行結果如下
+
+<a href="https://imgur.com/QmcIOcN"><img src="https://i.imgur.com/QmcIOcN.png" title="source: imgur.com" width = "400px"/></a>
+
+
+[1]:https://docs.microsoft.com/zh-tw/previous-versions/office/troubleshoot/office-developer/automate-excel-from-visual-c
--- processExcel.cs
+++ processExcel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace csharp_example
 {
@@ -11,10 +14,43 @@ namespace csharp_example
         public static void excel01()
         {
             //https://docs.microsoft.com/zh-tw/previous-versions/office/troubleshoot/office-developer/automate-excel-from-visual-c
-            Microsoft.Office.Interop.Excel.Application oXL;
-            Microsoft.Office.Interop.Excel._Workbook oWB;
-            Microsoft.Office.Interop.Excel._Worksheet oSheet;
-            Microsoft.Office.Interop.Excel.Range oRng;
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            Excel.Range oRng;
+            try
+            {
+                //Start Excel and get Application object.
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                //Get a new workbook.
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+
+                //Add table headers going cell by cell.
+                oSheet.Cells[1, 1] = "First Name";
+                oSheet.Cells[1, 2] = "Last Name";
+                oSheet.Cells[1, 3] = "Full Name";
+                oSheet.Cells[1, 4] = "Salary";
+
+                //Manipulate a variable number of columns for Quarterly Sales Data.
+
+                //Make sure Excel is visible and give the user control
+                //of Microsoft Excel's lifetime.
+                oXL.Visible = true;
+                oXL.UserControl = true;
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+            }
         }
     }
 }

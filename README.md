6292450f646954352b2c8cbeea79d16115d57cb0
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-03-11 15:10:46 +0800

    Patch example

--- README.md
+++ README.md
@@ -4,7 +4,25 @@
 
 執行結果如下
 
-<a href="https://imgur.com/8k0Nn5S"><img src="https://i.imgur.com/8k0Nn5S.png" title="source: imgur.com" width ="400px" /></a>
+<a href="https://imgur.com/IYM1gMv"><img src="https://i.imgur.com/IYM1gMv.png" title="source: imgur.com" width="400px" /></a>
+
+
+由c#程式產生，資料範圍沒有內容，但打印程式ranger是正確的
+
+<a href="https://imgur.com/aMwaDm9"><img src="https://i.imgur.com/aMwaDm9.png" title="source: imgur.com" width="400px" /></a>
+
+
+
+採用VBA設定方式如下
+```
+    ActiveChart.ChartArea.Select
+    Application.CutCopyMode = False
+    Application.CutCopyMode = False
+    ActiveChart.SetSourceData Source:=Range("E2:H6")
+    ActiveChart.FullSeriesCollection(1).XValues = "=工作表1!$A$2:$A$6"
+    ActiveChart.FullSeriesCollection(1).Name = "=""Q1"""
+    ActiveChart.FullSeriesCollection(2).Name = "=""Q2"""
+```    
 
 
 [1]:https://docs.microsoft.com/zh-tw/previous-versions/office/troubleshoot/office-developer/automate-excel-from-visual-c
--- processExcel.cs
+++ processExcel.cs
@@ -70,6 +70,7 @@ namespace csharp_example
                 oRng.EntireColumn.AutoFit();
 
                 //Manipulate a variable number of columns for Quarterly Sales Data.
+                DisplayQuarterlySales(oSheet);
 
                 //Make sure Excel is visible and give the user control
                 //of Microsoft Excel's lifetime.
@@ -86,6 +87,92 @@ namespace csharp_example
 
                 MessageBox.Show(errorMessage, "Error");
             }
+        }
+        private static void DisplayQuarterlySales(Excel._Worksheet oWS)
+        {
+            Excel._Workbook oWB;
+            Excel.Series oSeries;
+            Excel.Range oResizeRange;
+            Excel._Chart oChart;
+            String sMsg;
+            int iNumQtrs;
+
+            //Determine how many quarters to display data for.
+            for (iNumQtrs = 4; iNumQtrs >= 2; iNumQtrs--)
+            {
+                sMsg = "Enter sales data for ";
+                sMsg = String.Concat(sMsg, iNumQtrs);
+                sMsg = String.Concat(sMsg, " quarter(s)?");
+
+                DialogResult iRet = MessageBox.Show(sMsg, "Quarterly Sales?",
+                MessageBoxButtons.YesNo);
+                if (iRet == DialogResult.Yes)
+                    break;
+            }
+
+            sMsg = "Displaying data for ";
+            sMsg = String.Concat(sMsg, iNumQtrs);
+            sMsg = String.Concat(sMsg, " quarter(s).");
+            MessageBox.Show(sMsg, "Quarterly Sales");
+
+            //Starting at E1, fill headers for the number of columns selected.
+            oResizeRange = oWS.get_Range("E1", "E1").get_Resize(Missing.Value, iNumQtrs);
+            oResizeRange.Formula = "=\"Q\" & COLUMN()-4 & CHAR(10) & \"Sales\"";
+
+            //Change the Orientation and WrapText properties for the headers.
+            oResizeRange.Orientation = 38;
+            oResizeRange.WrapText = true;
+
+            //Fill the interior color of the headers.
+            oResizeRange.Interior.ColorIndex = 36;
+
+            //Fill the columns with a formula and apply a number format.
+            oResizeRange = oWS.get_Range("E2", "E6").get_Resize(Missing.Value, iNumQtrs);
+            oResizeRange.Formula = "=RAND()*100";
+            oResizeRange.NumberFormat = "$0.00";
+
+            //Apply borders to the Sales data and headers.
+            oResizeRange = oWS.get_Range("E1", "E6").get_Resize(Missing.Value, iNumQtrs);
+            oResizeRange.Borders.Weight = Excel.XlBorderWeight.xlThin;
+
+            //Add a Totals formula for the sales data and apply a border.
+            oResizeRange = oWS.get_Range("E8", "E8").get_Resize(Missing.Value, iNumQtrs);
+            oResizeRange.Formula = "=SUM(E2:E6)";
+            oResizeRange.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).LineStyle
+            = Excel.XlLineStyle.xlDouble;
+            oResizeRange.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).Weight
+            = Excel.XlBorderWeight.xlThick;
+
+            //Add a Chart for the selected data.
+            oWB = (Excel._Workbook)oWS.Parent;
+            oChart = (Excel._Chart)oWB.Charts.Add(Missing.Value, Missing.Value,
+            Missing.Value, Missing.Value);
+
+            //Use the ChartWizard to create a new chart from the selected data.
+            oResizeRange = oWS.get_Range("E2:E6", Missing.Value).get_Resize(Missing.Value, (iNumQtrs+1));
+
+
+            oChart.ChartWizard(oResizeRange, Excel.XlChartType.xl3DColumn, Missing.Value,
+            Excel.XlRowCol.xlColumns, Missing.Value, Missing.Value, Missing.Value,
+            Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            oSeries = (Excel.Series)oChart.SeriesCollection(1);
+            oSeries.XValues = oWS.get_Range("A2", "A6");
+            for (int iRet = 1; iRet <= (iNumQtrs-1); iRet++)
+            {
+                oSeries = (Excel.Series)oChart.SeriesCollection(iRet);
+                String seriesName;
+                seriesName = "=\"Q";
+                seriesName = String.Concat(seriesName, iRet);
+                seriesName = String.Concat(seriesName, "\"");
+                oSeries.Name = seriesName;
+            }
+            oChart.Location(Excel.XlChartLocation.xlLocationAsObject, oWS.Name);
+
+            //Move the chart so as not to cover your data.
+            oResizeRange = (Excel.Range)oWS.Rows.get_Item(10, Missing.Value);
+            oWS.Shapes.Item("Chart 1").Top = (float)(double)oResizeRange.Top;
+            oResizeRange = (Excel.Range)oWS.Columns.get_Item(2, Missing.Value);
+            oWS.Shapes.Item("Chart 1").Left = (float)(double)oResizeRange.Left;
         }
     }
 }

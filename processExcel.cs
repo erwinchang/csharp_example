1c5621880af94843177e13c5879812aac8906dde
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-03-11 12:03:45 +0800

    Add processExcel.cs

---
+++ processExcel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_example
+{
+    class processExcel
+    {
+        public static void excel01()
+        {
+            //https://docs.microsoft.com/zh-tw/previous-versions/office/troubleshoot/office-developer/automate-excel-from-visual-c
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            Microsoft.Office.Interop.Excel.Range oRng;
+        }
+    }
+}

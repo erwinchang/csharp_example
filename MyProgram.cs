916f2ae173cf571166cb58a23048afcfb71e1083
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-04-01 11:03:35 +0800

    Patch example

---
+++ MyProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_example
+{
+    class MyProgram
+    {
+    }
+    public class Example
+    {
+        public void Main()
+        {
+            Console.WriteLine("test main");
+        }
+    }
+}

2f8a81f99d4c5f2b6595864fbc8dbfe940a8bb08
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-03 11:22:49 +0800

    patch example

--- Program.cs
+++ Program.cs
@@ -14,6 +14,25 @@ namespace csharp_example
         [STAThread]
         static void Main()
         {
+            //https://docs.microsoft.com/zh-tw/dotnet/api/system.appdomain.friendlyname?view=net-6.0
+            AppDomain root = AppDomain.CurrentDomain;
+            AppDomainSetup setup = new AppDomainSetup();
+            setup.ApplicationBase = root.SetupInformation.ApplicationBase + @"MyAppSubfolder\";
+
+            AppDomain domain = AppDomain.CreateDomain("MyDomain", null, setup);
+            Console.WriteLine("Application base of {0}:\r\n\t{1}", root.FriendlyName, root.SetupInformation.ApplicationBase);
+            Console.WriteLine("Application base of {0}:\r\n\t{1}", domain.FriendlyName, domain.SetupInformation.ApplicationBase);
+            Console.WriteLine("");
+
+            // Output to the console.
+            Console.WriteLine("Host domain: " + AppDomain.CurrentDomain.FriendlyName);
+            Console.WriteLine("New domain: " + domain.FriendlyName);
+            Console.WriteLine("Application base is: " + domain.BaseDirectory);
+            Console.WriteLine("Relative search path is: " + domain.RelativeSearchPath);
+            Console.WriteLine("Shadow copy files is set to: " + domain.ShadowCopyFiles);
+            AppDomain.Unload(domain);
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # csharp_example
+
+
+-[從應用程式域取出安裝資訊][3]
+每個應用程式定義域的執行個體都包含這兩個屬性及 AppDomainSetup 資訊  
+可以從使用 System.AppDomain 類別的應用程式定義域擷取安裝資訊  
+
+
+- [AppDomain 類別][1]  
+表示應用程式定義域，也就是應用程式執行的獨立環境。 此類別無法獲得繼承。  
+
+- [AppDomain.FriendlyName 屬性][2]
+取得應用程式定義域的易記名稱
+
+## AppDomain.FriendlyName 屬性
+
+
+取得應用程式定義域的易記名稱
+
+
+執行結果如下
+```
+Application base of csharp_example.exe:              => root.FriendlyName
+	D:\gitWork\github\csharp_example\bin\Debug\      => root.SetupInformation.ApplicationBase
+Application base of MyDomain:
+	D:\gitWork\github\csharp_example\bin\Debug\MyAppSubfolder\
+
+Host domain: csharp_example.exe
+New domain: MyDomain
+Application base is: D:\gitWork\github\csharp_example\bin\Debug\MyAppSubfolder\
+Relative search path is: 
+Shadow copy files is set to: False	
+```
+
+
+[1]:https://docs.microsoft.com/zh-tw/dotnet/api/system.appdomain?view=net-6.0
+[2]:https://docs.microsoft.com/zh-tw/dotnet/api/system.appdomain.friendlyname?view=net-6.0
+[3]:https://docs.microsoft.com/zh-tw/dotnet/framework/app-domains/retrieve-setup-information

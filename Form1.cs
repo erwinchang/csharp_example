e9feb4dcc07604cb9800406c4e46bf6a7c3567ee
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-04 10:58:58 +0800

    patch menu

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,22 @@ namespace csharp_example
 {
     public partial class Form1 : Form
     {
+        //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.notifyicon?view=netframework-4.8
+        //private System.Windows.Forms.NotifyIcon notifyIcon1;
+        //private System.Windows.Forms.ContextMenu contextMenu1;
+        //private System.Windows.Forms.MenuItem menuItem1;
+        //private System.ComponentModel.IContainer components;
+
         public Form1()
         {
             InitializeComponent();
+            //this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+            //this.menuItem1 = new System.Windows.Forms.MenuItem();
+        }
+
+        private void menuStrip1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }

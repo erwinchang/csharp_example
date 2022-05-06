8678a2ec597c94634255dd193a397763747f9d5c
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-06 13:50:56 +0800

    patch Utilities/Nlog

--- Utilities/NlogEvent.cs
+++ Utilities/NlogEvent.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NLog;
+using NLog.Targets;
+
 namespace Utilities
 {
-    class NlogEventTarget
+    class NlogEventTarget : TargetWithLayout
     {
+        //https://nlog-project.org/documentation/v4.0.0/html/T_NLog_Targets_TargetWithLayout.htm
+        //Write(LogEventInfo)	Writes logging event to the log target.classes.
+        protected override void Write(LogEventInfo logEvent)
+        {
+
+        }
     }
 }

6c8213939a6dfc93239fecdb5a85a983375248c4
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-11 14:10:40 +0800

    add LoggingView.cs

---
+++ Utilities/Controls/LoggingView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Controls
+{
+    public partial class LoggingView : Component
+    {
+        public LoggingView()
+        {
+            InitializeComponent();
+        }
+
+        public LoggingView(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}

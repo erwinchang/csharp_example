936887d764261c2bd8c1fde1eb697b8cec6a318d
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-11 16:15:40 +0800

    LogForm add loggingView

--- WatchDog/LogForm.Designer.cs
+++ WatchDog/LogForm.Designer.cs
@@ -29,12 +29,33 @@ namespace WatchDog
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loggingView1 = new Utilities.Controls.LoggingView();
+            this.SuspendLayout();
+            // 
+            // loggingView1
+            // 
+            this.loggingView1.FormattingEnabled = true;
+            this.loggingView1.ItemHeight = 15;
+            this.loggingView1.Location = new System.Drawing.Point(12, 12);
+            this.loggingView1.MaxEntriesInListBox = 0;
+            this.loggingView1.Name = "loggingView1";
+            this.loggingView1.Size = new System.Drawing.Size(776, 409);
+            this.loggingView1.TabIndex = 0;
+            // 
+            // LogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loggingView1);
+            this.Name = "LogForm";
             this.Text = "LogForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Utilities.Controls.LoggingView loggingView1;
     }
 }

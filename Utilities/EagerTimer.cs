905ecb8ab84922255e2444d93666720ea54a149f
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-11 14:17:53 +0800

    patch EagerTimer

--- Utilities/EagerTimer.cs
+++ Utilities/EagerTimer.cs
@@ -5,21 +5,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Utilities
 {
-    public partial class EagerTimer : Component
+    public class EagerTimer : Timer
     {
-        public EagerTimer()
-        {
-            InitializeComponent();
-        }
-
-        public EagerTimer(IContainer container)
-        {
-            container.Add(this);
-
-            InitializeComponent();
-        }
+        public EagerTimer() : base() {}
+        public EagerTimer(double interval) : base(interval) { }
     }
 }

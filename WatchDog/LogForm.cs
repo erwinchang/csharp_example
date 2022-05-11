e7a2fc22888ceb3d703eae23cc4f0c8e50f735cd
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-11 16:24:22 +0800

    patch LogForm.cs

--- WatchDog/LogForm.cs
+++ WatchDog/LogForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Utilities.Controls;
 
 namespace WatchDog
 {
     public partial class LogForm : Form
     {
+        public LoggingView LoggingView { get { return loggingView; } }
+
         public LogForm()
         {
             InitializeComponent();

bb64a6ce1668c150d23eced63583bb45c966f27a
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-11 14:14:37 +0800

    add EagerTimer.cs

---
+++ Utilities/EagerTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public partial class EagerTimer : Component
+    {
+        public EagerTimer()
+        {
+            InitializeComponent();
+        }
+
+        public EagerTimer(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}

584e422049a854cf5f0735a64e4c835668981105
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-12 15:13:51 +0800

    patch ApplicationWatcher

--- WatchdogLib/ApplicationWatcher.cs
+++ WatchdogLib/ApplicationWatcher.cs
@@ -4,19 +4,37 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace WatchdogLib
 {
     public class ApplicationWatcher
     {
         private readonly Stopwatch _sleepStopwatch;
+        private readonly Logger _logger;
         public List<ApplicationHandler> ApplicationHandlers { get; set; }
 
         public ApplicationWatcher(Logger logger)
         {
             ApplicationHandlers = new List<ApplicationHandler>();
             _sleepStopwatch = new Stopwatch();
+            var asyncWorkerMonitor = new AsyncWorker(MonitorJob) { Name = "ApplicationWatcher" };
+            asyncWorkerMonitor.Start();
+            _logger = logger;
+        }
+
+        private bool MonitorJob()
+        {
+            // Walk through list of applications to see which ones are running
+            _sleepStopwatch.Restart();
+            foreach (var applicationHandler in ApplicationHandlers.ToArray())
+            {
+                //applicationHandler.Check();
+            }
+            Thread.Sleep(Math.Max(0, 500 - (int)_sleepStopwatch.ElapsedMilliseconds));
+            return true;
         }
     }
 }

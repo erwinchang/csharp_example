ed9d130ed00fac82393fc788bf0e9a979fc24508
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-12 15:24:04 +0800

    patch ApplicationHandler

--- WatchdogLib/ApplicationHandler.cs
+++ WatchdogLib/ApplicationHandler.cs
@@ -8,5 +8,26 @@ namespace WatchdogLib
 {
     public class ApplicationHandler
     {
+        public List<ProcessHandler> ProcessHandlers { get; set; }
+        public bool Active { get; set; }
+
+        public void Check()
+        {
+            if (!Active) return;
+            // Check if  new unmonitored process is running 
+            HandleDuplicateProcesses();
+            HandleNonResponsiveProcesses();
+        }
+
+        //移除相同名稱的process
+        public void HandleDuplicateProcesses()
+        {
+            //Loop through the running processes in with the same name 
+        }
+
+        private void HandleNonResponsiveProcesses()
+        {
+
+        }
     }
 }

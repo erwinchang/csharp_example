5b211ae71dec5cb30cf357d6f7c5263fdfc92e96
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-12 13:34:29 +0800

    patch ApplicationWatcher

--- WatchdogLib/ApplicationWatcher.cs
+++ WatchdogLib/ApplicationWatcher.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,13 @@ namespace WatchdogLib
 {
     public class ApplicationWatcher
     {
+        private readonly Stopwatch _sleepStopwatch;
+        public List<ApplicationHandler> ApplicationHandlers { get; set; }
+
+        public ApplicationWatcher(Logger logger)
+        {
+            ApplicationHandlers = new List<ApplicationHandler>();
+            _sleepStopwatch = new Stopwatch();
+        }
     }
 }

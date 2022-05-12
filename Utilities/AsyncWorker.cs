60edcfcdd25a913b39fd92001a82137361ff0ebe
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-12 14:41:09 +0800

    patch AsyncWorker

--- Utilities/AsyncWorker.cs
+++ Utilities/AsyncWorker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Utilities
@@ -24,8 +25,11 @@ namespace Utilities
         private volatile State _state = State.Stopped;
         private volatile State _requestedState = State.Stopped;
 
+        private readonly EventWaiter _eventWaiter = new EventWaiter();
+
         private readonly object _lock = new object();
         private readonly AsyncWorkerJob _workerJob;
+        private System.Threading.Tasks.Task _workerTask;
         public string Name { get; set; }
 
         public AsyncWorker(AsyncWorkerJob workerJob)
@@ -41,6 +45,34 @@ namespace Utilities
                 if (_state == State.Stopped)
                 {
                     _requestedState = _state = State.Running;
+                    _eventWaiter.Reset();
+
+
+                    // prefer using Task.Factory.StartNew for .net 4.0. For .net 4.5 Task.Run is the better option.
+                    _workerTask = Task.Factory.StartNew(x =>
+                    {
+                        while (true)
+                        {
+                            if (_state == State.Stopped) break;
+
+                            bool haveMoreWork = false;
+                            if (_state == State.Running)
+                            {
+                                haveMoreWork = _workerJob();
+
+                                // Check if state has been changed in workerJob thread.
+                                if (_requestedState != _state && _requestedState == State.Stopped)
+                                {
+                                    _state = _requestedState;
+                                    break;
+                                }
+                            }
+
+                            if (!haveMoreWork || _state == State.Suspended) _eventWaiter.WaitOne(Timeout.Infinite);
+                            _state = _requestedState;
+                        }
+                    }, CancellationToken.None, TaskCreationOptions.LongRunning);
+
                 }
             }
         }

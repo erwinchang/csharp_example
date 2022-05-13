5a84049a30ae0fed549168eb422eb8f65a6d5c88
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-14 00:03:52 +0800

    add Heartbeat

---
+++ WatchdogClientLib/Heartbeat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchdogClientLib
+{
+    public class Heartbeat
+    {
+        private const string PipeName = "named_pipe_watchdog";
+    }
+}

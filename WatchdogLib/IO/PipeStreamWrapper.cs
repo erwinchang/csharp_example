548369813fcffd8b2b0e11d22209695903f16051
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-13 12:10:28 +0800

    add PipeStreamWrapper

---
+++ WatchdogLib/IO/PipeStreamWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchdogLib.IO
+{
+    /// <summary>
+    /// Wraps a <see cref="PipeStream"/> object to read and write .NET CLR objects.
+    /// </summary>
+    /// <typeparam name="TReadWrite">Reference type to read from and write to the pipe</typeparam>
+    public class PipeStreamWrapper<TReadWrite> : PipeStreamWrapper<TReadWrite, TReadWrite>
+        where TReadWrite : class
+    {
+
+    }
+    /// <summary>
+    /// Wraps a <see cref="PipeStream"/> object to read and write .NET CLR objects.
+    /// </summary>
+    /// <typeparam name="TRead">Reference type to read from the pipe</typeparam>
+    /// <typeparam name="TWrite">Reference type to write to the pipe</typeparam>
+    public class PipeStreamWrapper<TRead, TWrite>
+        where TRead : class
+        where TWrite : class
+    {
+
+    }
+}

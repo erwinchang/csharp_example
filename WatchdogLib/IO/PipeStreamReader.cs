ea6b6dff19d294717af553c1b70ebe24cdf019f1
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-13 12:14:11 +0800

    add PipeStreamReader

---
+++ WatchdogLib/IO/PipeStreamReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchdogLib.IO
+{
+    /// <summary>
+    /// Wraps a <see cref="PipeStream"/> object and reads from it.  Deserializes binary data sent by a <see cref="PipeStreamWriter{T}"/>
+    /// into a .NET CLR object specified by <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">Reference type to deserialize data to</typeparam>
+    public class PipeStreamReader<T> where T : class
+    {
+        /// <summary>
+        /// Gets the underlying <c>PipeStream</c> object.
+        /// </summary>
+        public PipeStream BaseStream { get; }
+        /// <summary>
+        /// Gets a value indicating whether the pipe is connected or not.
+        /// </summary>
+        public bool IsConnected { get; private set; }
+
+        /// <summary>
+        /// Constructs a new <c>PipeStreamReader</c> object that reads data from the given <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">Pipe to read from</param>
+        public PipeStreamReader(PipeStream stream)
+        {
+            BaseStream = stream;
+            IsConnected = stream.IsConnected;
+        }
+    }
+}

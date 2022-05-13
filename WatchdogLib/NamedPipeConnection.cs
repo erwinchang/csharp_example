cf99bc107b1632bc01bc7210508c74505d44901e
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-13 09:10:32 +0800

    add NamedPipeConnection

---
+++ WatchdogLib/NamedPipeConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchdogLib
+{
+    /// <summary>
+    /// Represents a connection between a named pipe client and server.
+    /// </summary>
+    /// <typeparam name="TRead">Reference type to read from the named pipe</typeparam>
+    /// <typeparam name="TWrite">Reference type to write to the named pipe</typeparam>
+    public class NamedPipeConnection<TRead, TWrite>
+        where TRead : class
+        where TWrite : class
+    {
+    }
+}

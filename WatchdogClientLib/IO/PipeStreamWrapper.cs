87e6255eb798d5b5684d7530f0b063b11b3a30dc
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-16 13:36:53 +0800

    patch pipstream

--- WatchdogClientLib/IO/PipeStreamWrapper.cs
+++ WatchdogClientLib/IO/PipeStreamWrapper.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WatchdogClient.IO
 {
-    class PipeStreamWrapper
+    /// <summary>
+    /// Wraps a <see cref="PipeStream"/> object to read and write .NET CLR objects.
+    /// </summary>
+    /// <typeparam name="TReadWrite">Reference type to read from and write to the pipe</typeparam>
+    public class PipeStreamWrapper<TReadWrite> : PipeStreamWrapper<TReadWrite, TReadWrite>
+        where TReadWrite : class
     {
+        /// <summary>
+        /// Constructs a new <c>PipeStreamWrapper</c> object that reads from and writes to the given <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">Stream to read from and write to</param>
+        public PipeStreamWrapper(PipeStream stream) : base(stream)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Wraps a <see cref="PipeStream"/> object to read and write .NET CLR objects.
+    /// </summary>
+    /// <typeparam name="TRead">Reference type to read from the pipe</typeparam>
+    /// <typeparam name="TWrite">Reference type to write to the pipe</typeparam>
+    public class PipeStreamWrapper<TRead, TWrite>
+        where TRead : class
+        where TWrite : class
+    {
+        /// <summary>
+        /// Gets the underlying <c>PipeStream</c> object.
+        /// </summary>
+        public PipeStream BaseStream { get; private set; }
+
+
+
+        private readonly PipeStreamReader<TRead> _reader;
+        private readonly PipeStreamWriter<TWrite> _writer;
+
+        /// <summary>
+        /// Constructs a new <c>PipeStreamWrapper</c> object that reads from and writes to the given <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">Stream to read from and write to</param>
+        public PipeStreamWrapper(PipeStream stream)
+        {
+            BaseStream = stream;
+            _reader = new PipeStreamReader<TRead>(BaseStream);
+            _writer = new PipeStreamWriter<TWrite>(BaseStream);
+        }
     }
 }
--- WatchdogClientLib/IO/PipeStreamWriter.cs
+++ WatchdogClientLib/IO/PipeStreamWriter.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WatchdogClient.IO
 {
-    class PipeStreamWriter
+    /// <summary>
+    /// Wraps a <see cref="PipeStream"/> object and writes to it.  Serializes .NET CLR objects specified by <typeparamref name="T"/>
+    /// into binary form and sends them over the named pipe for a <see cref="PipeStreamWriter{T}"/> to read and deserialize.
+    /// </summary>
+    /// <typeparam name="T">Reference type to serialize</typeparam>
+    public class PipeStreamWriter<T> where T : class
     {
+        /// <summary>
+        /// Gets the underlying <c>PipeStream</c> object.
+        /// </summary>
+        public PipeStream BaseStream { get; private set; }
+
+        private readonly BinaryFormatter _binaryFormatter = new BinaryFormatter();
+
+        /// <summary>
+        /// Constructs a new <c>PipeStreamWriter</c> object that writes to given <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">Pipe to write to</param>
+        public PipeStreamWriter(PipeStream stream)
+        {
+            BaseStream = stream;
+        }
     }
 }

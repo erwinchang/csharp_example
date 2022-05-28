83839d525a021a106df09783a03a797a25e68696
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-05-28 21:05:19 +0800

    patch Configuration

--- WatchdogLib/Configuration.cs
+++ WatchdogLib/Configuration.cs
@@ -7,27 +7,27 @@ namespace WatchdogLib
     {
         public enum RebootModes
         {
-            ShutDown,
-            Reboot,
-            PowerOff,
+            ShutDown,      
+            Reboot,        
+            PowerOff,      
             HybridShutdown,
         };
 
 
         public enum ForceModes
         {
-            Normal,
-            Force,
-            ForceIfHung,
+             Normal,     
+             Force,      
+             ForceIfHung,
         };
 
         public enum RebootAfterTimeSlotModes
-        {
+        { 
             FirstOccasion,
             TryNextDay
         };
 
-
+    
         public List<ApplicationHandlerConfig> ApplicationHandlers { get; set; }
         public bool ShowTrayIcon { get; set; }
         public bool PeriodicReboot { get; set; }
@@ -57,33 +57,34 @@ namespace WatchdogLib
 
     public class ApplicationHandlerConfig
     {
-        public int NonResponsiveInterval { get; set; }
-        public string ApplicationPath { get; set; }
-        public string ApplicationName { get; set; }
-        public bool UseHeartbeat { get; set; }
-        public bool GrantKillRequest { get; set; }
-        public uint HeartbeatInterval { get; set; }
-        public int MaxProcesses { get; set; }
-        public int MinProcesses { get; set; }
-        public bool Active { get; set; }
+        public int NonResponsiveInterval    { get; set; }
+        public string ApplicationPath       { get; set; }
+        public string ApplicationName       { get; set; }
+        public bool UseHeartbeat            { get; set; }
+        public bool GrantKillRequest        { get; set; }
+        public uint HeartbeatInterval       { get; set; }
+        public int MaxProcesses             { get; set; }
+        public int MinProcesses             { get; set; }
+        public bool Active                  { get; set; }
         public bool KeepExistingNoProcesses { get; set; }
 
-        public uint StartupMonitorDelay { get; set; }
+        public uint StartupMonitorDelay      { get; set; }
 
         public ApplicationHandlerConfig()
         {
-            NonResponsiveInterval = 20;
-            ApplicationPath = "";
-            ApplicationName = "";
-            UseHeartbeat = false;
-            GrantKillRequest = true;
-            HeartbeatInterval = 20;
-            MinProcesses = 1;
-            MaxProcesses = 10;
-            Active = false;
+            NonResponsiveInterval   = 20;
+            ApplicationPath         = "";
+            ApplicationName         = "";
+            UseHeartbeat            = false;
+            GrantKillRequest        = true;
+            HeartbeatInterval       = 20;
+            MinProcesses            = 1;
+            MaxProcesses            = 10;
+            Active                  = false;
             KeepExistingNoProcesses = true;
-            StartupMonitorDelay = 20;
+            StartupMonitorDelay     = 20;
         }
-
     }
+
+
 }

c34adc8da64c649088e30d3d0256be4459234815
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-06-10 10:30:52 +0800

    patch example

--- MyProgram.cs
+++ MyProgram.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace csharp_example
 {
@@ -22,6 +23,66 @@ namespace csharp_example
         public void Main()
         {
             Console.WriteLine("Example Main");
+            //https://www.codeguru.com/dotnet/using-sqlite-in-a-c-application/
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            CreateTable(sqlite_conn);
+            InsertData(sqlite_conn);
+            ReadData(sqlite_conn);
+        }
+
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=testdb.db;Version=3;New=True;Compress=True;");
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Open fail {0}", ex.Message);
+            }
+            return sqlite_conn;
+        }
+        static void CreateTable(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE SampleTable(Col1 VARCHAR(20), Col2 INT)";
+            string Createsql1 = "CREATE TABLE SampleTable1(Col1 VARCHAR(20), Col2 INT)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = Createsql1;
+            sqlite_cmd.ExecuteNonQuery();
+        }
+        static void InsertData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test Text ', 1);";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test1 Text1 ', 2);";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test2 Text2 ', 3);";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable1(Col1, Col2) VALUES ('Test3 Text3 ', 3);";
+            sqlite_cmd.ExecuteNonQuery();
+        }
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM SampleTable";
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader0 = sqlite_datareader.GetString(0);
+                int myreader1 = sqlite_datareader.GetInt32(1);
+                Console.WriteLine($"{myreader0} {myreader1}");
+            }
+            conn.Close();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # csharp_example
+
+
+## sqlite example
+
+
+### 1-0 先用sqlitestudio建立test.db  
+
+### 1-1 [C#安裝System.Data.SQLite套件][2]  
+
+### 1-2 測試  
+
+[Using SQLite in a C# Application][1]  
+
+#### 1-2-1 建立連線  
+
+```
+SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=testdb.db;Version=3;New=True;Compress=True;");
+sqlite_conn.Open();
+
+```
+
+#### 1-2-2 建立table  
+```
+SQLiteCommand sqlite_cmd;
+string Createsql = "CREATE TABLE SampleTable(Col1 VARCHAR(20), Col2 INT)";
+sqlite_cmd = sqlite_conn.CreateCommand();
+sqlite_cmd.CommandText = Createsql;
+```
+
+<a href="https://imgur.com/AzKrQu0"><img src="https://i.imgur.com/AzKrQu0.png" title="source: imgur.com" /></a>
+
+
+#### 1-2-3 寫入資料  
+
+```
+SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test Text ', 1);";
+sqlite_cmd.ExecuteNonQuery();
+xxx
+
+```
+
+<a href="https://imgur.com/hRsXorn"><img src="https://i.imgur.com/hRsXorn.png" title="source: imgur.com" /></a>
+
+
+#### 1-2-4 讀出資料  
+
+```
+SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+sqlite_cmd.CommandText = "SELECT * FROM SampleTable";
+sqlite_datareader = sqlite_cmd.ExecuteReader();
+while (sqlite_datareader.Read()){
+string myreader0 = sqlite_datareader.GetString(0);
+int myreader1 = sqlite_datareader.GetInt32(1);
+Console.WriteLine($"{myreader0} {myreader1}");
+}
+```
+
+測試結果如下  
+```
+Test Text 1
+Test1 Text1 2
+Test2 Text2 3
+```
+
+
+[1]:https://www.codeguru.com/dotnet/using-sqlite-in-a-c-application/
+[2]:https://www.ruyut.com/2021/06/nuget-install.html

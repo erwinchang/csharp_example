d824bd008c4b7265446260e638a5a4475f26a20d
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-06-29 17:34:56 +0800

    patch ex06

--- MyProgram.cs
+++ MyProgram.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Dapper;
 using System.Data.SQLite;
+using System.Data;
 
 namespace csharp_example
 {
@@ -27,9 +28,12 @@ namespace csharp_example
         public void Main()
         {
             Console.WriteLine("Example ..");
-            ex01();
+            //ex01();
             //ex02();
             //ex03();
+            //ex04();
+            //ex05();
+            ex06();
         }
 
         //https://www.tpisoftware.com/tpu/articleDetails/1046
@@ -82,6 +86,51 @@ namespace csharp_example
                 {
                     Console.WriteLine($"{cust.CustomerID},{cust.CompanName},{cust.Address},{cust.Citys},{cust.Phone}");
                 }
+            }
+            Console.ReadLine();
+        }
+        public void ex04()
+        {
+            string strConnection = "Data Source=db.db;";
+            using (SQLiteConnection conn = new SQLiteConnection(strConnection))
+            {
+                conn.Open();
+                string sql = "select * from Customers";
+                var customers = conn.QueryFirstOrDefault(sql);
+            }
+            Console.ReadLine();
+        }
+        public void ex05()
+        {
+            string strConnection = "Data Source=db.db;";
+            using (SQLiteConnection conn = new SQLiteConnection(strConnection))
+            {
+                conn.Open();
+                string sql = "select * from Customers; select * from users";
+                using (var results = conn.QueryMultiple(sql))
+                {
+                    var customers = results.Read().ToList();
+                    var users = results.Read().ToList();
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public void ex06()
+        {
+            string strConnection = "Data Source=db.db;";
+            using (SQLiteConnection conn = new SQLiteConnection(strConnection))
+            {
+                conn.Open();
+                //string sql = "select * from Customers; select * from users";
+                //var results = conn.Query("spGetUser", new { Id = 1 }, commandType: CommandType.StoredProcedure);
+                var p = new DynamicParameters();
+                p.Add("@a", 11);
+                p.Add("@b", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                p.Add("@c", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+                conn.Execute("spMagicProc", p, commandType: CommandType.StoredProcedure);
+                int b = p.Get<int>("@b");
+                int c = p.Get<int>("@c");
             }
             Console.ReadLine();
         }

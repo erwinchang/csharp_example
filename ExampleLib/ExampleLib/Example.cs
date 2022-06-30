fed0e3c5c8155ed614cfde2f44e01c521b985e54
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-06-30 15:43:32 +0800

    patch example

---
+++ ExampleLib/ExampleLib/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExampleLib
+{
+    public class Example
+    {
+        //https://marcus116.blogspot.com/2019/01/unittest-nunit.html
+        public string Reverse(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}

1a32a2026381c03101d4408cd7ec5e345d1e269f
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-06-30 16:06:07 +0800

    patch example

--- ExampleLib/ExampleLibMSTests/UnitTest1.cs
+++ ExampleLib/ExampleLibMSTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ExampleLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 //https://docs.microsoft.com/zh-tw/visualstudio/test/getting-started-with-unit-testing?view=vs-2022&tabs=dotnet%2Cmstest
@@ -6,9 +7,13 @@ namespace ExampleLibMSTests
     [TestClass]
     public class UnitTest1
     {
+        private const string Expected = "654321";
         [TestMethod]
         public void TestMethod1()
         {
+            Example ex1 = new Example();
+            var result = ex1.Reverse("123456");
+            Assert.AreEqual(Expected, result);
         }
     }
 }

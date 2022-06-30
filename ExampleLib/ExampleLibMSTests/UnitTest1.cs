b9fce105fc2cbc4f62428bed1ea2edff120edee1
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-06-30 15:55:16 +0800

    add MSTests

---
+++ ExampleLib/ExampleLibMSTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//https://docs.microsoft.com/zh-tw/visualstudio/test/getting-started-with-unit-testing?view=vs-2022&tabs=dotnet%2Cmstest
+namespace ExampleLibMSTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

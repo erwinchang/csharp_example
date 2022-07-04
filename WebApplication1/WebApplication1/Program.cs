d73ba35e18ff66ae330c1683caef45eddb6755c2
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-07-04 14:34:12 +0800

    patch example

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -7,20 +7,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//https://blog.johnwu.cc/article/asp-net-core-3-application-lifetime.html
 namespace WebApplication1
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Output("[Program] Start");
+
+            Output("[Program] Create HostBuilder");
+            var hostBuilder = CreateHostBuilder(args);
+
+            Output("[Program] Build Host");
+            var host = hostBuilder.Build();
+
+            Output("[Program] Run Host");
+            host.Run();
+
+            Output("[Program] End");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => {
+                    Output("[Program] hostBuilder.ConfigureServices - Called");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder
+                    .ConfigureServices(services =>{
+                        Output("[Program] webBuilder.ConfigureServices - Called");
+                    })
+                    .Configure(app => {
+                        Output("[Program] webBuilder.Configure - Called");
+                    })
+                    .UseStartup<Startup>();
+                }
+        );
+        public static void Output(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}] {message}");
+        }
     }
 }

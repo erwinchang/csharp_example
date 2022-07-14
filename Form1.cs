28f20620ab28c513e971d4fd5fea3ef8551d7fef
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-07-14 10:59:28 +0800

    patch example

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Faker;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,49 @@ namespace csharp_example
 {
     public partial class Form1 : Form
     {
+        BindingList<Part> listOfParts;
         public Form1()
         {
             InitializeComponent();
+            listOfParts = new BindingList<Part>();
+
+            listOfParts.Add(new Part("Widget", 1234));
+            listOfParts.Add(new Part("Gadget", 5647));
+            listBox1.DataSource = listOfParts;
+            listBox1.DisplayMember = "PartName";
+        }      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listOfParts.Add(new Part("T11", 5647));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listOfParts.Clear();
+        }
+    }
+
+    //https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.bindinglist-1?redirectedfrom=MSDN&view=net-6.0
+    // A simple business object for example purposes.
+    public class Part
+    {
+        private string name;
+        private int number;
+        public Part() { }
+        public Part(string nameForPart, int numberForPart)
+        {
+            PartName = nameForPart;
+            PartNumber = numberForPart;
+        }
+        public string PartName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int PartNumber
+        {
+            get { return number; }
+            set { number = value; }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # csharp_example
+
+## 採用BindingList來更新ListBox
+
+1.定義BindingList<Part> listOfParts
+2.定義ListBox顯示內容
+- 設定物件Part的PartName為顯示
+- listBox1.DisplayMember = "PartName";
+
+初始化
+```
+listOfParts = new BindingList<Part>();
+
+listOfParts.Add(new Part("Widget", 1234));
+listOfParts.Add(new Part("Gadget", 5647));
+listBox1.DataSource = listOfParts;
+listBox1.DisplayMember = "PartName";
+```	
+
+Add
+對listOfParts操作，將會自動更新ListBox
+```
+listOfParts.Add(new Part("T11", 5647));
+```
+
+Clear
+```
+listOfParts.Clear();
+```
+
+
+[BindingList<T> Class][1]
+[1]:https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.bindinglist-1?redirectedfrom=MSDN&view=net-6.0

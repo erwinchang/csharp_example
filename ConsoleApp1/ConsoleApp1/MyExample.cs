6208b770215f5efa0c5295e56587abf5693584a1
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-07-20 16:46:24 +0800

    patch example

---
+++ ConsoleApp1/ConsoleApp1/MyExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //https://dotblogs.com.tw/yc421206/2011/01/04/20574
+    public class MyExample
+    {
+        public static void ThreadProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.WriteLine("ThreadProc: {0}, {1}", i, Thread.CurrentThread.ThreadState);
+                Thread.Sleep(1000);
+            }
+        }
+        public void Test()
+        {
+            Debug.WriteLine("Main thread: Start a second thread.");
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            t.IsBackground = true;
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Debug.WriteLine("Main thread: Do some work");
+                Thread.Sleep(1);
+            }
+            Debug.WriteLine("Press Enter to end program.");
+            Console.ReadLine();
+            Debug.WriteLine("Exit");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # csharp_example
+
+## Thread /IsBackground差別  
+
+[背景執行緒與前景執行緒的差別][1]  
+
+Thread.IsBackground=false  
+此時為背景，好處若主程式離開，此時背景程式會馬上自動關閉離開  
+因此主程式可以加入Join來確保Thread有正常執行完成  
+
+<a href="https://imgur.com/v2zoe2J"><img src="https://i.imgur.com/v2zoe2J.png" title="source: imgur.com" width="400px" /></a>
+
+
+Thread.IsBackground=true  
+此時為前景，好處若主程式離開，此時會等Thread執行完才會離開    
+
+<a href="https://imgur.com/TU1qf1O"><img src="https://i.imgur.com/TU1qf1O.png" title="source: imgur.com" width="400px" /></a>
+
+[1]:https://dotblogs.com.tw/yc421206/2011/01/04/20574

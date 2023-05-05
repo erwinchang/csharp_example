8e1be12ed8ce31b861f329e5b54145c001c83f75
Erwin Chang <m9207216@mail.ntust.edu.tw>
2023-05-05 15:26:11 +0800

    patch example

--- WpfApp1/README.md
+++ WpfApp1/README.md
@@ -9,5 +9,11 @@
 gridMain.DataContext = BindingData.vm;
 ```
 
+### 2. 直接由MyUserControl設定相關變數連結
+
+```
+public partial class MyUserControl : UserControl, INotifyPropertyChanged
+```
+
 
 [1]:https://ascii-iicsa.blogspot.com/2014/08/dependencyproperty.html
--- WpfApp1/WpfApp1/MyUserControl.xaml.cs
+++ WpfApp1/WpfApp1/MyUserControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,12 +20,66 @@ namespace WpfApp1
     /// <summary>
     /// MyUserControl.xaml 的互動邏輯
     /// </summary>
-    public partial class MyUserControl : UserControl
+    public partial class MyUserControl : UserControl, INotifyPropertyChanged
     {
         public MyUserControl()
         {
             InitializeComponent();
-            gridMain.DataContext = BindingData.vm;
+            DataContext = this;
+        }
+
+        private String contextText;
+        public String ContentText
+        {
+            get
+            {
+                return contextText;
+            }
+            set
+            {
+                SetProperty(ref contextText, value, "ContentText");
+            }
+        }
+
+        private String header;
+        public String Header
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                SetProperty(ref header, value, "Header");
+            }
+        }
+        private String backgroundColor;
+        public String BackgroundColor
+        {
+            get
+            {
+                return backgroundColor;
+            }
+            set
+            {
+                SetProperty(ref backgroundColor, value, "BackgroundColor");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        {
+            if (object.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+            return true;
         }
     }
 }

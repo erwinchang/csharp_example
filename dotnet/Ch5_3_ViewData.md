2b4b244c302ba907284e80ebb541a5834b3f5fd6
Erwin Chang <m9207216@mail.ntust.edu.tw>
2023-05-11 22:34:55 +0800

    patch CH5_3 example

---
+++ dotnet/Ch5_3_ViewData.md
@@ -0,0 +1,10 @@
+# mvc example 1
+
+
+## Ch5
+
+### CH5-3 ViewData
+
+1. 新增PassDataController.cs  
+2. 在PassDataController.cs/ Index點右鍵，新增檢視 (將會產生Views/PassData/Index.cshtml)  
+
---
+++ dotnet/MvcFriends/Controllers/PassDataController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcFriends.Models;
+
+namespace MvcFriends.Controllers
+{
+    public class PassDataController : Controller
+    {
+        private readonly ILogger<PassDataController> _logger;
+
+        public PassDataController(ILogger<PassDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

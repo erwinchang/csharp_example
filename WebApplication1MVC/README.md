dd8da432e933e9b1d143a517be1a365acbcb7b16
Erwin Chang <m9207216@mail.ntust.edu.tw>
2023-05-12 13:14:58 +0800

    create db

--- WebApplication1MVC/README.md
+++ WebApplication1MVC/README.md
@@ -8,3 +8,20 @@
 <a href="https://imgur.com/gZ4Y9Ht"><img src="https://i.imgur.com/gZ4Y9Ht.png" title="source: imgur.com" /></a>
 
 <a href="https://imgur.com/VwPxSr6"><img src="https://i.imgur.com/VwPxSr6.png" title="source: imgur.com" /></a>
+
+3.NUGet主控台
+
+```
+PM> Add-Migration InitialDB #建立Migrations及程式
+Build started...
+Build succeeded.
+To undo this action, use Remove-Migration.
+
+PM> Update-Database #執行程式，會產生FriendDB1資料庫
+Build started...
+Build succeeded.
+Done.
+PM> 
+```
+
+<a href="https://imgur.com/HayIZaW"><img src="https://i.imgur.com/HayIZaW.png" title="source: imgur.com" /></a>
---
+++ WebApplication1MVC/WebApplication1MVC/Migrations/20230512050933_InitialDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1MVC.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Friend",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Mobile = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friend", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friend");
+        }
+    }
+}

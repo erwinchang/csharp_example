f14299557e9aef17774654449f14d24fddb3fd8f
Erwin Chang <m9207216@mail.ntust.edu.tw>
2023-08-21 15:45:28 +0800

    add Scaffolding auto create

---
+++ dotnet/CH6-4-Boostrap/Data/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CH6_4_Boostrap.Models;
+
+namespace CH6_4_Boostrap.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CH6_4_Boostrap.Models.Employee> Employee { get; set; }
+    }
+}

8c8e7e627ee6fe8a3b4e0cfab5f4eae597fcda4c
Erwin Chang <m9207216@mail.ntust.edu.tw>
2023-08-21 15:44:36 +0800

    add Employee.cs

--- dotnet/CH6-4-Boostrap.md
+++ dotnet/CH6-4-Boostrap.md
@@ -6,3 +6,15 @@
 Microsoft.EntityFrameworkCore.SqlServer 3.1.0
 Microsoft.EntityFrameworkCore.Tools 3.1.0
 ```
+
+### 2 建立CRUD樣板
+
+1. 新增Models/Employee.cs
+2. Controller資料夾按滑鼠右鍵->加入->新增Scaffold項目
+
+<img src="https://i.imgur.com/QIJ3EMu.png" title="" />
+
+<img src="https://i.imgur.com/3XsmjQw.png" title="" />
+
+<img src="https://i.imgur.com/9vwURkc.png" title="" />
+
---
+++ dotnet/CH6-4-Boostrap/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CH6_4_Boostrap.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Title { get; set; }
+    }
+}

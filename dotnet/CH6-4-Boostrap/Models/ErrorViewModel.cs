482d96376cc414b2caa029423719f696ac418ca4
Erwin Chang <m9207216@mail.ntust.edu.tw>
2023-08-21 15:20:34 +0800

    add CH6-4-Boostrap

---
+++ dotnet/CH6-4-Boostrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CH6_4_Boostrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

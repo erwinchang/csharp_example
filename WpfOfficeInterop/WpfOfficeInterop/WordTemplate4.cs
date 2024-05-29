91c203082b6cad7acc1d05e3d05bb26a3394fb12
Erwin Chang <m9207216@mail.ntust.edu.tw>
2024-05-29 13:46:47 +0800

    add DocumentFormat.OpenXml v3.0.0

---
+++ WpfOfficeInterop/WpfOfficeInterop/WordTemplate4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfOfficeInterop
+{
+    public class WordTemplate4
+    {
+        //https://learn.microsoft.com/en-us/office/open-xml/word/structure-of-a-wordprocessingml-document?tabs=cs
+        public static bool Test(ref string strRet)
+        {
+            bool flag = true;
+            strRet = string.Empty;
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                flag = true;
+                strRet = string.Format("[] {0}", ex.Message);
+            }
+            return flag;
+        }
+    }
+}
